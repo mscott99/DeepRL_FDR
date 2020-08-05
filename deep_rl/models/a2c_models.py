fix gradient clipping

deep_rl/models/a2c_models.py
@@ -197,8 +197,8 @@ class Small_A2C_FDR(Model):
         config.critic_hidden_units = (32,32)
         config.use_gae = False
         config.entropy_weight = 0.01
-        config.alternate = True
-        config.check_for_alternation_callback = check_alternate_by_cFDR
+        config.alternate = False
+        #config.check_for_alternation_callback = check_alternate_by_cFDR
         #config.check_for_alternation = lambda *params: check_alternate_by_cFDR(0.2, 0.2, *params)
         config.rollout_length = 5
         #config.gradient_clip = 0.5
@@ -206,8 +206,8 @@ class Small_A2C_FDR(Model):
         #config.log_interval = 1e4
         config.max_steps = 2e5
         #config.log_keywords = [("action",0),("critic_loss",0),("actor_loss",0) ,("dFDR_critic",100),("cFDR_critic",100), ("dFDR_actor", 100), ("cFDR_actor", 100), ("OL_critic",100),("OR_critic",1),("Base_Theta_critic",0),("OL_actor",100),("OR_actor",1),("Base_Theta_actor",0)]
-        config.log_keywords = [("action",0),("critic_loss",0),("actor_loss",0) ,("dFDR_critic",100), ("dFDR_actor", 100), ("OL_critic",100),("OR_critic",1),("OL_actor",100),("OR_actor",1)]
-        config.tag = "mountain_car_debug"
+        #config.log_keywords = [("action",0),("critic_loss",0),("actor_loss",0) ,("dFDR_critic",100), ("dFDR_actor", 100), ("OL_critic",100),("OR_critic",1),("OL_actor",100),("OR_actor",1)]
+        #config.tag = "mountain_car_debug"
         self.agent = FDRA2CAgent
         config.merge(kwargs)
         self.config = config
@@ -217,7 +217,7 @@ class FDR_A2C_Entropy(Small_A2C_FDR):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         config= self.config
-        config.entropy_weight = 0.1
+        config.entropy_weight = 0.01
         #Override when arguments are given from the running configuration
         config.merge(kwargs)
 
@@ -237,7 +237,6 @@ class FDR_A2C_ADAM(Small_A2C_FDR):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         config= self.config
-        config.alternate=False
         config.actor_body = FCBody(config.state_dim, hidden_units=config.actor_hidden_units, gate=config.gate)
         config.critic_body = FCBody(config.state_dim, hidden_units=config.critic_hidden_units, gate=config.gate)
         config.actor_optimizer_fn = lambda params, logger: torch.optim.Adam(params, config.actor_lr)

hyper_tuning.py
@@ -115,13 +115,15 @@ def purge_model_logging(dir_name, file_pattern):
 
 
 def run_single(params, values_in_tag=[], model_index=None, eval_with_final_score=True, save_all_params=False, num_evals=1000, **kwargs):
-    complete_tag = params['tag']
+    initial_tag = complete_tag = params['tag']
     if len(values_in_tag) > 0:
         complete_tag = complete_tag +'_' + ('_').join([str(key) + '_' + str(params[key]) for key in values_in_tag])
 
     if model_index is not None:
         complete_tag= complete_tag + "_model_" + str(model_index)
         params['model_index'] = model_index #important for deletion of files by leaderboard
+
+    params['tag'] = complete_tag
 
     save_folder = 'data/' + params['group_tag']+'/'
     if not os.path.exists(save_folder):
@@ -137,6 +139,8 @@ def run_single(params, values_in_tag=[], model_index=None, eval_with_final_score
     if save_all_params:
         with open(save_folder + complete_tag+ str(".json"), 'w+') as file:
             json.dump([complete_tag, score, params], file, default=lambda o:o.__name__)
+
+    params['tag'] = initial_tag
 
     return score, model
 
@@ -823,7 +827,7 @@ def tune_Adam_cheetah():
         'critic_hidden_units':(100, 100),
         'gate':torch.relu,
         'log_keywords': [['episodic_return_train', 0], ['episode_count', 0], ['lr_critic', 0], ['lr_actor', 0],],
-        'group_tag':'normalised_cheetah_Adam_v0',
+        'group_tag':'normalised_cheetah_Adam_v2',
         'tag':'run',
         'max_steps': 1e6,
         'game': 'HalfCheetah-v2',